773b7299a81c5b3e5008d8095291f104c19c5e41
geyixin <geyixin777@163.com>
2018-06-01 22:46:33 +0800

    1

---
+++ 3sum/3sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def threeSum(self, nums):
+        n = len(nums)
+        m = []
+        for i in range(n-2):
+            for j in range(i+1, n-1):
+                for p in range(j+1, n):
+                    if (nums[i]+nums[j]+nums[p]) == 0:
+                        m.append([nums[i], nums[j], nums[p]])
+        for k in range(len(m)-1):
+            if len(m[k]) != 0:
+                for t in range(k+1, len(m)):
+                    if len(m[t]) != 0:
+                        if set(m[t]) == set(m[k]):
+                            m[t] = []
+        while [] in m:
+            m.remove([])
+        return m
